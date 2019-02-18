b7d9b54b8ad64c4dadee6d0ddb2669c2ad3a8c8c
yangwenmai <yangwen.yw@gmail.com>
2019-02-18 22:13:04 +0800

    feat(weekly_digest): new pull_request

--- pull_requests.go
+++ pull_requests.go
@@ -2,101 +2,20 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"strings"
 	"time"
 
 	"github.com/google/go-github/github"
-	"golang.org/x/oauth2"
 )
 
-var (
-	accessToken = flag.String("access_token", "", "Github API access token")
-	owner       = flag.String("owner", "", "Github owner name")
-	repo        = flag.String("repo", "", "Github owner's repo")
-	endDate     = flag.String("end_date", "", "end date")
-	interval    = flag.Int64("interval", 7, "past days base on end date")
-)
-
-const timeLayout = "2006-01-02 15:04:05"
-
-func main() {
-	flag.Parse()
-	checkParams()
-
-	tailDate, _ := time.Parse(timeLayout, *endDate)
-	headDate := tailDate.Add(time.Duration(-*interval*24) * time.Hour)
-
-	prs := genPRS()
+func printPullRequests(ctx context.Context, headDate, tailDate time.Time) string {
+	prs := fetchPRS(ctx)
 	intervalPRs := filterPRS(prs, headDate, tailDate)
-	pullRequestsString := printPRSReport(intervalPRs, headDate, tailDate, formatWords())
-	
-	// create a issue
-	title := fmt.Sprintf("Weekly Digest (%d %s, %d - %d %s, %d)",
-		headDate.Day(), headDate.Month(), headDate.Year(), tailDate.Day(), tailDate.Month(), tailDate.Year())
-
-	body := ""
-	bodyHead := fmt.Sprintf("Here's the **Weekly Digest** for [*%s/%s*](https://github.com/%s/%s):\n", *owner, *repo, *owner, *repo)
-
-	body += bodyHead
-
-	if len(pullRequestsString) > 0 {
-		body += "\n --- \n"
-		body += pullRequestsString
-	}
-	body += "\n --- \n"
-	body += "\n"
-
-	createIssue(title, body, "", []string{"weekly-digest"})
+	return wrapPRSReport(intervalPRs, headDate, tailDate, formatWords())
 }
 
-func checkParams() {
-	if *accessToken == "" {
-		panic("Github API access token can not be empty.")
-	}
-	if *owner == "" {
-		panic("Github owner name can not be empty.")
-	}
-	if *repo == "" {
-		panic("Github owner's repo can not be empty.")
-	}
-	if *endDate == "" {
-		*endDate = time.Now().Format(timeLayout)
-	}
-	if *interval > 30 {
-		panic("the max interval value is 30")
-	}
-}
-
-func formatWords() string {
-	lastStr := ""
-	switch *interval {
-	case 7:
-		lastStr = "weeks"
-		break
-	case 14:
-		lastStr = "2 weeks"
-		break
-	case 21:
-		lastStr = "3 weeks"
-		break
-	case 30:
-		lastStr = "months"
-		break
-	default:
-		lastStr = fmt.Sprintf("%d days", *interval)
-	}
-	return lastStr
-}
-func genPRS() []*github.PullRequest {
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: *accessToken},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
-
+func fetchPRS(ctx context.Context) []*github.PullRequest {
 	// FIXME:暂时只取 100 个PR（有可能30天的PR是超过100的），后续再优化
 	listOpts := github.ListOptions{PerPage: 100}
 	opts := &github.PullRequestListOptions{State: "all", ListOptions: listOpts}
@@ -107,28 +26,6 @@ func genPRS() []*github.PullRequest {
 	}
 
 	return prs
-}
-
-func createIssue(title, body, assignee string, labels []string) *github.Issue {
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: *accessToken},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
-	_ = client
-	input := &github.IssueRequest{
-		Title:    &title,
-		Body:     &body,
-		Assignee: &assignee,
-		Labels:   &labels,
-	}
-	
-	issue, _, err := client.Issues.Create(ctx, *owner, *repo, input)
-	if err != nil {
-		panic(err)
-	}
-	return issue
 }
 
 func filterPRS(prs []*github.PullRequest, headDate, tailDate time.Time) []*github.PullRequest {
@@ -150,7 +47,7 @@ func filterPRS(prs []*github.PullRequest, headDate, tailDate time.Time) []*githu
 	return intervalPRs
 }
 
-func printPRSReport(intervalPRs []*github.PullRequest, headDate, tailDate time.Time, lastStr string) string {
+func wrapPRSReport(intervalPRs []*github.PullRequest, headDate, tailDate time.Time, lastStr string) string {
 	pullRequestsString := "# PULL REQUESTS\n"
 	if len(intervalPRs) == 0 {
 		pullRequestsString += fmt.Sprintf("Last %s (%s~%s), no pull requests were created, updated or merged.\n",
---
+++ weekly_digest.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+var (
+	accessToken = flag.String("access_token", "", "Github API access token")
+	owner       = flag.String("owner", "", "Github owner name")
+	repo        = flag.String("repo", "", "Github owner's repo")
+	endDate     = flag.String("end_date", "", "end date")
+	interval    = flag.Int64("interval", 7, "past days base on end date")
+)
+
+var (
+	client *github.Client
+)
+
+const timeLayout = "2006-01-02 15:04:05"
+
+func init() {
+	client = NewClient()
+}
+
+func main() {
+	flag.Parse()
+	checkParams()
+
+	tailDate, _ := time.Parse(timeLayout, *endDate)
+	headDate := tailDate.Add(time.Duration(-*interval*24) * time.Hour)
+	// create a issue
+	title := fmt.Sprintf("Weekly Digest (%d %s, %d - %d %s, %d)",
+		headDate.Day(), headDate.Month(), headDate.Year(), tailDate.Day(), tailDate.Month(), tailDate.Year())
+
+	pullRequestsString := printPullRequests(headDate, tailDate)
+	body := ""
+	bodyHead := fmt.Sprintf("Here's the **Weekly Digest** for [*%s/%s*](https://github.com/%s/%s):\n", *owner, *repo, *owner, *repo)
+
+	body += bodyHead
+
+	if len(pullRequestsString) > 0 {
+		body += "\n --- \n"
+		body += pullRequestsString
+	}
+	body += "\n --- \n"
+	body += "\n"
+
+	fmt.Println(title, body)
+	// createIssue(title, body, "", []string{"weekly-digest"})
+}
+
+func NewClient() *github.Client {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: *accessToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client = github.NewClient(tc)
+	return client
+}
+
+func checkParams() {
+	if *accessToken == "" {
+		panic("Github API access token can not be empty.")
+	}
+	if *owner == "" {
+		panic("Github owner name can not be empty.")
+	}
+	if *repo == "" {
+		panic("Github owner's repo can not be empty.")
+	}
+	if *endDate == "" {
+		*endDate = time.Now().Format(timeLayout)
+	}
+	if *interval > 30 {
+		panic("the max interval value is 30")
+	}
+}
+
+func formatWords() string {
+	lastStr := ""
+	switch *interval {
+	case 7:
+		lastStr = "weeks"
+		break
+	case 14:
+		lastStr = "2 weeks"
+		break
+	case 21:
+		lastStr = "3 weeks"
+		break
+	case 30:
+		lastStr = "months"
+		break
+	default:
+		lastStr = fmt.Sprintf("%d days", *interval)
+	}
+	return lastStr
+}
+
+func createIssue(title, body, assignee string, labels []string) *github.Issue {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: *accessToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	_ = client
+	input := &github.IssueRequest{
+		Title:    &title,
+		Body:     &body,
+		Assignee: &assignee,
+		Labels:   &labels,
+	}
+
+	issue, _, err := client.Issues.Create(ctx, *owner, *repo, input)
+	if err != nil {
+		panic(err)
+	}
+	return issue
+}
